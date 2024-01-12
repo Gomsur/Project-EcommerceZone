e95b2195cdc5c278b0dc586e3a10da1652d298bd
robert@eddiebengtsson.se <mdabdurrahmanchowdhury1122@gmail.com>
2024-01-13 00:06:01 +0600

    Update model

--- webapp1/models.py
+++ webapp1/models.py
@@ -42,11 +42,11 @@ class products(models.Model):
     product_name = models.CharField(max_length=2000)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     product_price = models.CharField(max_length=2000)
-    image = models.ImageField(upload_to='uploads/product_image')
-    image2 = models.ImageField(upload_to='uploads/product_image', null=True, blank=True, default='')
-    image3 = models.ImageField(upload_to='uploads/product_image', null=True, blank=True, default='')
-    image4 = models.ImageField(upload_to='uploads/product_image', null=True, blank=True, default='')
-    image5 = models.ImageField(upload_to='uploads/product_image', null=True, blank=True, default='')
+    image = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True, default='')
+    image3 = models.ImageField(null=True, blank=True, default='')
+    image4 = models.ImageField(null=True, blank=True, default='')
+    image5 = models.ImageField(null=True, blank=True, default='')
     description = models.TextField()
 
     def __str__(self):
